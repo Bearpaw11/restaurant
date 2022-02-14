de80ab2ae3056d766fe1f9a00e0e1c91845c0216
Chris Behrens <chrisbehrens@yahoo.com>
2022-02-14 14:11:55 -0700

    Updated header image and styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Tavern from "../../assets/tavern.jpeg";
+import Modern from "../../assets/modern.jpeg";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>The Modest Modern</h1>
         <button>Cart</button>
       </header>
       <div className={classes[`main-image`]}>
-        <img src={Tavern} alt="Tavern" />
+        <img src={Modern} alt="Modern Restaurant" />
       </div>
     </React.Fragment>
   );
