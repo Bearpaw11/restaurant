eb01dad5a41a33f45d4572684eb88bbd9f73ca5b
Chris Behrens <chrisbehrens@yahoo.com>
2022-02-16 16:55:25 -0700

    added CartCtx and numberOfCartItems variables. The CartCts get the statue using CartContext and nuumberOfCartItems calculated the number of items added to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,13 @@
-import react from 'react'
+import react, {useContext} from 'react'
 import classes from "./HeaderCartButton.module.css"
-import React from "react"
 import CartIcon from '../Cart/CartIcon'
+import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
-
+    const CartCtx = useContext(CartContext)
+    const numberOfCartItems = CartCtx.items.reduce((curNumber, item) => {
+      return curNumber + item.amount;
+    }, 0);
   return (
       <button className ={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
@@ -12,7 +15,7 @@ const HeaderCartButton = (props) => {
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>
-          3
+          {numberOfCartItems}
         </span>
       </button>
     
