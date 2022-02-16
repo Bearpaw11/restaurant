823a3d24a55d7bae0b5173da7952e6cac590a4b6
Chris Behrens <chrisbehrens@yahoo.com>
2022-02-16 15:16:38 -0700

    added cart-context.js and CartProvider.js. These willb e used to mange the state of the cart. Wrapped App.js with CartProvider to provide access to the state throughout the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react"
 import {useState} from 'react';
-import './App.css';
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals"
 import Cart from './components/Cart/Cart'
-
+import CartProvider from './store/CartProvider'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -17,13 +16,13 @@ function App() {
     setCartIsShown(false)
   }
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
