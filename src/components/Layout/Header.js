c3e9ff224fc21f5222ce63940d4126dbb8288b71
Chris Behrens <chrisbehrens@yahoo.com>
2022-02-16 11:11:40 -0700

    feat: added conditionl rendering for cart. This iss then passeed via props to Cart and Modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>The Modest Modern</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes[`main-image`]}>
         <img src={Modern} alt="Modern Restaurant" />
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom";
 
 
 const Backdrop = props => {
-  return <div className={classes.backdrop} />
+  return <div className={classes.backdrop} onClick={props.onClose}/>
 }
 
 const ModalOverlay = props => {
   return (
-  <div className={classes.modal}>
+  <div className={classes.modal} >
     <div className={classes.content}>{props.children}</div>
   </div>
   );
@@ -21,7 +21,7 @@ const Modal = props => {
 
   return  (
     <React.Fragment>
-      {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </React.Fragment>
   )
