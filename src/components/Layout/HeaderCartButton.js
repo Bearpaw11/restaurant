711c8d8b3d2df958ec15bdd3482da2301639eb9c
Chris Behrens <chrisbehrens@yahoo.com>
2022-02-17 21:27:36 -0700

    feat: added bump className to button in HeaderCartButton.js. This use useEffect to only add when cartAount is updated

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import react, {useContext} from 'react'
+import react, {useContext, useEffect, useState} from 'react'
 import classes from "./HeaderCartButton.module.css"
 import CartIcon from '../Cart/CartIcon'
 import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
+  const [buttonBump, setButtonBump] = useState(false)
     const CartCtx = useContext(CartContext)
-    const numberOfCartItems = CartCtx.items.reduce((curNumber, item) => {
+    const { items } = CartCtx
+    const numberOfCartItems = items.reduce((curNumber, item) => {
       return curNumber + item.amount;
     }, 0);
+
+   
+    const btnClasses =`${classes.button} ${buttonBump ? classes.bump : ''}`
+
+    useEffect(() => {
+      if(items.length === 0) {
+        return
+      } 
+      setButtonBump(true);
+      const timer = setTimeout(() => {
+        setButtonBump(false)
+      }, 300)
+      
+      return () => {
+        clearTimeout(timer)
+      }
+    }, [items]);
+
   return (
-      <button className ={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon/>
         </span>
