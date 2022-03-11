c44abe9b6728d88cef69414c8e948d335a8f544e
Chris Behrens <chrisbehrens@yahoo.com>
2022-03-10 20:56:27 -0700

    added isLoading state to AvaialbleMeals. This displays loading meaassage on screen while meals are loading

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,35 +3,11 @@ import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-// const DUMMY_MEALS = [
-//   {
-//     id: 'm1',
-//     name: 'Sushi',
-//     description: 'Finest fish and veggies',
-//     price: 22.99,
-//   },
-//   {
-//     id: 'm2',
-//     name: 'Schnitzel',
-//     description: 'A german specialty!',
-//     price: 16.5,
-//   },
-//   {
-//     id: 'm3',
-//     name: 'Barbecue Burger',
-//     description: 'American, raw, meaty',
-//     price: 12.99,
-//   },
-//   {
-//     id: 'm4',
-//     name: 'Green Bowl',
-//     description: 'Healthy...and green...',
-//     price: 18.99,
-//   },
-// ];
+
 
 const AvailablelMeals = () => {
   const [meals, setMeals] = useState([])
+  const [isLoading, setIsloading] = useState(true)
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch("https://meals-b48a5-default-rtdb.firebaseio.com/meals.json")
@@ -48,9 +24,18 @@ const AvailablelMeals = () => {
 
         }
         setMeals(loadedMeals)
+        setIsloading(false)
     };
     fetchMeals();
   }, []);
+
+if (isLoading) {
+  return (
+    <section className={classes.MealsLoading} >
+      <p>loading....</p>
+    </section>
+  )
+}
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -63,6 +48,7 @@ const AvailablelMeals = () => {
   ));
 
   return (
+  
     <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
